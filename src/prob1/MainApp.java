98010389cf7ba48b0ecb6ea1898e671c9cc88cbd
S401 <ysi1246@daum.net>
2016-07-11 15:45:49 +0900

    20160711

--- src/prob1/MainApp.java
+++ src/prob1/MainApp.java
@@ -2,6 +2,20 @@ package prob1;
 
 public class MainApp {
 	public static void main(String[] args) {
-		int[] data = {1,3,5,8,9,11,15,19,18,20,30,33,31};
+		int[] data = { 1, 3, 5, 8, 9, 11, 15, 19, 18, 20, 30, 33, 31 };
+		mulAdd(data);
+	}
+
+	public static void mulAdd(int[] d) {
+		int count = 0;
+		int mAdd = 0;
+		for (int i = 0; i < d.length; i++) {
+			if ((d[i] % 3) == 0) {
+				count += 1;
+				mAdd = mAdd + d[i];
+			}
+		}
+		System.out.println("주어진 배열에서 3의 배수의 개수	=> " + count);
+		System.out.println("주어진 배열에서 3의 배수의 합	=> " + mAdd);
 	}
 }
--- src/prob2/Prob2.java
+++ src/prob2/Prob2.java
@@ -1,8 +1,20 @@
 package prob2;
 
+import java.util.Scanner;
+
 public class Prob2 {
 	public static void main(String[] args) {
 		/* 구현코드 */
+		int count = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("금액 : ");
+		int money = scanner.nextInt();
+
+		while (money == 1) {
+			if(money>50000){
+				money = money%50000;
+				count++;
+			}
+		}
 	}
 }
-
